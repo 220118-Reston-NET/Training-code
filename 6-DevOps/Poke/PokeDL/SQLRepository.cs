643bcc539622160ebad970782b5de3a3c53c4385
Scifiler-sys <pagdilaostephen@gmail.com>
2022-03-01 12:19:05 -0500

    Added Frontend demo

--- 6-DevOps/Poke/PokeDL/SQLRepository.cs
+++ 6-DevOps/Poke/PokeDL/SQLRepository.cs
@@ -108,7 +108,7 @@ namespace PokeDL
         }
 
         public List<Pokemon> GetAllPokemon()
-        {
+        {;
             List<Pokemon> listOfPokemon = new List<Pokemon>();
 
             string sqlQuery = @"select * from Pokemon";
